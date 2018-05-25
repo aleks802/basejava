0e9fcbeeff0cca0fc78a79bab294147bb94c1bec
Aleksandr Marchenko <sanya80291@gmail.com>
2018-05-25 21:27:52 +0300

    implemented the save method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,12 +5,35 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        Arrays.fill(storage, new Resume());
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        int index = getIndex(r.getUuid());
+
+        if (index >= 0) {
+            System.out.println("Такое резюме уже существует");
+        } else if (size == storage.length) {
+            System.out.println(" превышен лимит");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    private int getIndex(String uuid) {
+
+        for (int i = 0; i < size; i += 1) {
+
+            if (uuid.equals(storage[i].getUuid())) {
+
+                return i;
+            }
+        }
+        return -1;
     }
 
     Resume get(String uuid) {
@@ -28,6 +51,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
