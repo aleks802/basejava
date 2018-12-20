2f7a7fd1509a6a95e1d39a00a0bb657fa61d2351
aleks.marchenko <aleks.marchenko802@gmail.com>
2018-12-20 22:03:16 +0200

    refactoring and implementation AbstractFileStorage

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -1,7 +1,5 @@
 package com.urise.webapp.model;
 
-import sun.swing.SwingUtilities2;
-
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Objects;
@@ -40,6 +38,14 @@ public class Resume implements Comparable<Resume> {
 
     public Section getSection(SectionType type) {
         return sections.get(type);
+    }
+
+    public void addContact(ContactType type, String value) {
+        contacts.put(type, value);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
     }
 
     @Override
